512369e44ca5ab76651273c9b13c049beae8a8a0
Ahmed Ali <ahmedali.96@hotmail.com>
2021-05-31 11:39:45 -0400

    updated fetch

--- src/core/context/AppState.js
+++ src/core/context/AppState.js
@@ -25,6 +25,9 @@ const AppState = (props) => {
       `https://api.nomics.com/v1/currencies/ticker?key=${apiKey}&interval=${interval}&convert=${localCurrency}&per-page=100&page=1`,
       {
         mode: "cors",
+        headers: {
+          "access-control-allow-origin": "*",
+        },
       }
     )
       .then((response) => response.json())
